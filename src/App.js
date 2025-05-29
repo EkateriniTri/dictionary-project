ae02380bb91e2af5110c17186badd1680d455486
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2025-05-29 20:01:43 +0200

    added phonetic and logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Dictionary from "./Dictionary";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Agape.Dictionary</header>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
         <main>
           <Dictionary defaultKeyword="agape" />
         </main>
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Result(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word} </h2>
+        {props.results.phonetic && (
+          <div className="Phonetic">
+            <span>/{props.results.phonetic} /</span>
+          </div>
+        )}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
